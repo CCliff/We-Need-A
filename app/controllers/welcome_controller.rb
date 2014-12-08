1f3fa9ec7fc8b451c4d6c72e061d7b7a3d12571b
CCLiff <CLCliff30@gmail.com>
2014-12-07 21:26:03 -0500

    fixes bug

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,10 @@
 class WelcomeController < ApplicationController
 
 
-  get '/' do
-    binding.pry
-    erb :index
-  end
+  # get '/' do
+  #   binding.pry
+  #   erb :index
+  # end
 
   get '/console' do
     binding.pry
