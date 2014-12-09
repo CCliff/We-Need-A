515be03e9db02e37ad9807d629d02b0ba1ec25d1
CCLiff <CLCliff30@gmail.com>
2014-12-09 10:57:05 -0500

    attempting to fix database connection concurrency issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ require 'sinatra/base'
 
 class ApplicationController < Sinatra::Base
 
+  after { ActiveRecord::Base.connection.close }
+
   enable :sessions
 
   set :views, File.expand_path('../../views', __FILE__)
